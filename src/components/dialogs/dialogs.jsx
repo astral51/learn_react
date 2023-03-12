1288aa03c1c616d38167d6c1f68c0527d773c323
1astral_ <lenkin.ma@gmail.com>
2023-03-12 15:22:20 +0300

    made a sketch of the dialog page

--- src/components/dialogs/dialogs.jsx
+++ src/components/dialogs/dialogs.jsx
@@ -2,8 +2,29 @@ import s from './dialogs.module.css';
 
 const Dialogs = () =>{
 	return (
-		<div>
-			Dialogs
+		<div className={s.dialogs}>
+			<div className={s.dialogs_items}>
+				<div className={s.dialog + ' ' + s.active}>
+					Dmitriy
+				</div>
+				<div className={s.dialog}>
+					Sveta
+				</div>
+				<div className={s.dialog}>
+					Sasha
+				</div>
+				<div className={s.dialog}>
+					Gosha
+				</div>
+				<div className={s.dialog}>
+					Valera
+				</div>
+			</div>
+			<div className={s.messages}>
+				<div className={s.message}>Hi</div>
+				<div className={s.message}>How are u?</div>
+				<div className={s.message}>I hope, are u okey</div>
+			</div>
 		</div>
 	);
 }
