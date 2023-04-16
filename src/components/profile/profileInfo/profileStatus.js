53a191541037a09d15cb724d889dc8bd495a96f1
1astral_ <lenkin.ma@gmail.com>
2023-04-16 16:02:48 +0300

    refactoring profileStatus

--- src/components/profile/profileInfo/profileStatus.js
+++ src/components/profile/profileInfo/profileStatus.js
@@ -1,47 +1,40 @@
-import react from "react";
+import {useEffect, useState} from "react";
 
-class ProfileStatus extends react.Component{
+const ProfileStatus = (props) => {
 
-	state = {
-		editMode: false,
-		status: this.props.status,
-	}
+	let [editMode, setEditMode] = useState(false);
+	let [status, setStatus] = useState(props.status);
 
-	activateEditMode = () => {
-		this.setState({editMode: true});
-	}
-	deactivateEditMode = () => {
-		this.setState({editMode: false});
-		this.props.updateStatus(this.state.status);
-	}
-	onStatusChange = (e) => {
-		this.setState({
-			status: e.currentTarget.value,
-		});
-	}
+	useEffect(() => {
+		setStatus(props.status);
+	}, [props.status]);
 
-	componentDidUpdate(prevProps, prevState, snapshot) {
-		if (prevProps.status !== this.props.status){
-			this.setState({status: this.props.status,})
-		}
+	const activateEditMode = () => {
+		setEditMode(true);
+	}
+	const deactivateEditMode = () => {
+		setEditMode(false);
+		props.updateStatus(status);
+	}
+	const onStatusChange = (e) => {
+		setStatus(e.currentTarget.value);
 	}
 
-	render() {
-		return (
-			<div>
-				{!this.state.editMode
-					?
-					<div>
-						<span onDoubleClick={this.activateEditMode}>{this.props.status || '------'}</span>
-					</div>
-					:
-					<div>
-						<input onChange={this.onStatusChange} autoFocus={true} onBlur={this.deactivateEditMode} value={this.state.status}/>
-					</div>
-				}
-			</div>
-		);
-	}
+	return (
+		<div>
+			{!editMode
+				?
+				<div>
+					<span onDoubleClick={activateEditMode}>{props.status || '------'}</span>
+				</div>
+				:
+				<div>
+					<input onChange={onStatusChange} autoFocus={true} onBlur={deactivateEditMode}
+					       value={status}/>
+				</div>
+			}
+		</div>
+	);
 }
 
 
